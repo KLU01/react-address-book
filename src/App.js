7655ad39f79865d0155e33a48f63b443d7962247
kelly lu <klu001@citymail.cuny.edu>
2019-03-19 00:39:53 -0400

    add init state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    address: [
+      {
+        FirstName: "Cathy",
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+      },
+      {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362"
+      },
+      {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday: "8/3/1970",
+        Telephone: "200-661-9407"
+      },
+      {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967"
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Address Book</h1>
         </header>
       </div>
     );
